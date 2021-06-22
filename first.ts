04d5480a7b17712b49f47b158cdea5bd940e9761
Jawadul Alam <jawadprogrammingpro@gmail.com>
2021-06-22 17:25:04 +0600

    Add existing file

--- first.ts
+++ first.ts
@@ -1,1 +1,9 @@
-let myMoney = 1;
+let myMoney = 122;
+let age:number = 59;
+const club:string = "Real Madrid";
+let famous: Boolean;
+// the value of let can be used later but const value cant be used later
+function add (num1:number, num2:number){
+ return num1 + num2;
+}
+ add(5,6);
